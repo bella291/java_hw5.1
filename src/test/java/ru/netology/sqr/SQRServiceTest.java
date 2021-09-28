f0a121ab186aef0d5ec11a33b17e933d3301c04e
Bella Khamchieva <bella.kh29@gmail.com>
2021-09-28 12:11:50 +0300

    Parametrized test added

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,32 +1,32 @@
 package ru.netology.sqr;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
 
-    @Test
-    void shouldEnumerateRootsFrom4000to8000() {
+    @ParameterizedTest
+    @CsvSource(value = {"'shouldEnumerateRootFrom666to999', 666, 999, 6"})
+    void shouldEnumerateRootFrom10to99Squares(String testname, int lowLimit, int upLimit, int expected) {
         SQRService service = new SQRService();
-        int lowLimit = 4000;
-        int upLimit = 8000;
-        int expected = 26;
         int actual = service.rootEnumerate(lowLimit, upLimit);
 
         assertEquals(expected, actual);
     }
 
-        @Test
-        void shouldEnumerateRootsFrom500to900() {
-            SQRService service = new SQRService();
-            int lowLimit = 500;
-            int upLimit = 900;
-            int expected = 8;
-            int actual = service.rootEnumerate(lowLimit, upLimit);
+    @Test
+    void shouldEnumerateRootsFrom500to900() {
+        SQRService service = new SQRService();
+        int lowLimit = 500;
+        int upLimit = 900;
+        int expected = 8;
+        int actual = service.rootEnumerate(lowLimit, upLimit);
 
-            assertEquals(expected, actual);
-        }
+        assertEquals(expected, actual);
+    }
 
 
     @Test
@@ -35,7 +35,18 @@ class SQRServiceTest {
         int lowLimit = 1547;
         int upLimit = 4894;
         int expected = 30;
-        int actual= service.rootEnumerate(lowLimit, upLimit);
+        int actual = service.rootEnumerate(lowLimit, upLimit);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldEnumerateRootsFromMinus154to0() {
+        SQRService service = new SQRService();
+        int lowLimit = -154;
+        int upLimit = 0;
+        int expected = 0;
+        int actual = service.rootEnumerate(lowLimit, upLimit);
 
         assertEquals(expected, actual);
     }
